35de5ee8c6a280b5de6e1bd86c4d732b6d93e0b9
LightChaosman <lightchaosman@hotmail.com>
2015-04-16 23:32:14 +0200

    A class for Vector, Line and Rectangle.
    Geograph gives a code snippet that converts GeoData.js into an array of
    Line objects.

---
+++ Line.js
@@ -0,0 +1,53 @@
+/**
+ * Created by Helmond on 16-4-2015.
+ */
+
+function Line(p1,p2)
+{
+    this.p1 = p1;
+    this.p2 = p2;
+    this.d = p2.subtract(p1);
+    return(this);
+}
+
+//Returns null if the lines are collinear, or partially overlap.
+//Otherwise, returns an array [t,u] such that the intersection point lies at this.getPointAt(t) or l.getPointAt(u)
+// http://stackoverflow.com/questions/563198/how-do-you-detect-where-two-line-segments-intersect
+Line.prototype.getIntersection = function(l){//returns the intersection point between line l1 and l2
+    var A = this.p1;
+    var B = this.p2;
+    var C = l.p1;
+    var D = l.p2;
+    var p = A;
+    var r = B.subtract(A);
+    var q = C;
+    var s = D.subtract(C);
+    var qminp = q.subtract(p);
+    var rs = Vector.prototype.cross2D(r,s);
+    if(rs==0)//lines are colinear or paralel
+    {//We do not care if colinear lines overlap
+        return null ;
+    }
+
+    var t = Vector.prototype.cross2D(qminp,s)/rs;//if(t<0||t>1){return(null);}
+    var u = Vector.prototype.cross2D(qminp,r)/rs;//can be optimized by returning null if we already know t<0 || t>1
+    if(0 <= t && t<= 1 && 0<= u && u<= 1)
+    {
+        return([t,u]);
+    }
+    return null ;
+};
+
+//Returns the point on this line that lies at 100*t % from p1 to p2.
+Line.prototype.getPointAt = function(t){
+    return this.p1.add(this.d.scale(t));
+};
+
+//Returns at what side of the line
+//http://stackoverflow.com/questions/2752725/finding-whether-a-point-lies-inside-a-rectangle-or-not
+Line.prototype.sideOf = function(p){
+    var A = -(this.p2.y - this.p1.y);
+    var B = this.p2.x - this.p1.x;
+    var C = -(A * this.p1.x + B * this.p1.y)
+    return A * p.x + B * p.y + C
+};
---
+++ Rectangle.js
@@ -0,0 +1,57 @@
+/**
+ * Created by Helmond on 16-4-2015.
+ */
+function Rectangle(p1,p2,p3,p4){//the 4 points defining the rectangle. No check for right angles is done
+    //this.ps = [p1,p2,p3,p4];
+    this.ls = [new Line(p1,p2),new Line(p2,p3),new Line(p3,p4),new Line(p4,p1)];
+    return this;
+}
+
+//Returns true if the point p is inside this rectangle.
+Rectangle.prototype.isInside = function(p){
+    var sides = [];
+    for(var i = 0; i < 4; i++)
+    {
+        sides.push(this.ls[i].sideOf(p));
+    }
+    var s1 = sides[0];
+    for(var i = 1; i < 4; i++)
+    {
+        if(sides[i]!=s1)return false;
+    }
+    return true;
+};
+
+//Returns an array [t,u] such that the line defined by l.getPointAt(t) and l.getPoint(u) lies inside the rectangle.
+// Returns an empty array if the line does not overlap with the rectangle.
+Rectangle.prototype.getSubLineInside = function(l){
+
+    var lp1 = l.p1;
+    var lp2 = l.p2;
+    var in1 = this.isInside(lp1);
+    var in2 = this.isInside(lp2);
+    if(in1 && in2) return [0,1];
+    var ts = [];
+    for(var i = 0; i<4;i++)
+    {
+        var inter = l.getIntersection(this.ls[i])
+        if(inter!=null) ts.push(inter);
+    }
+    if(ts.length==0)return [];
+    if(ts.length==1)//1 point inside
+    {
+        var t = ts[0][0];
+        if(in1)
+        {
+            return [t,1];
+        }else
+        {
+            return [0,t];
+        }
+    }else//ic==2; -> !in1 && !in2;
+    {
+
+        if(ts[0][0]<ts[1][0])return([ts[0][0],ts[1][0]]);
+        return([ts[1][0],ts[0][0]]);
+    }
+};
