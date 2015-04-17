e37120a491d920f07eab5ac66a86026d214a14c8
Thom Hurks <thomhurks@gmail.com>
2015-04-17 19:03:06 +0200

    Cleanup in GeoJSON

--- GeoJSON.js
+++ GeoJSON.js
@@ -1,24 +1,29 @@
 function GeoJSON() {
+    "use strict";
     this.type = "FeatureCollection";
     this.features = [];
 }
 
 
-GeoJSON.prototype.addSegments = function(segments) {
-    for (var segment in segments) {
+GeoJSON.prototype.addSegments = function (segments) {
+    "use strict";
+    var segment;
+    for (segment in segments) {
         this.addSegment(segments[segment]);
     }
 };
 
-GeoJSON.prototype.addSegment = function(segment) {
+GeoJSON.prototype.addSegment = function (segment) {
+    "use strict";
     var feature = {
         "type": "Feature",
-        "geometry":{
+        "geometry": {
             "type": "LineString",
             "coordinates": [
                 [segment.p1.x, segment.p1.y],
                 [segment.p2.x, segment.p2.y]
-            ]},
+            ]
+        },
         "properties": {"edgeweight": segment.weight}
     };
 
