05ad425e3fa113ef19ab445b61f16b1801120911
LightChaosman <lightchaosman@hotmail.com>
2015-04-17 15:06:52 +0200

    Second simplification done. Fixed ill-defined dot product.

--- Preprocessor.js
+++ Preprocessor.js
@@ -34,11 +34,11 @@ function preProcess(geodata,epsilon)
     //returns a simplified version of the trajectory
     function simplify(trajectory,epsilon)
     {
-        var alpha = Math.PI*2/360*5;//5 degrees
+        var alpha = Math.PI*2/360*5;//90 degrees
         var traj1 = simplify1(trajectory,epsilon)
         var traj2 = simplify2(traj1,alpha);
         //return trajectory;
-        return traj1;
+        return traj2;
 
         function simplify1(trajectory, minlength)//Driemel algorithm
         {
@@ -63,8 +63,26 @@ function preProcess(geodata,epsilon)
         }
 
         function simplify2(trajectory, alpha){//straightens out any slight curves. all bends are more than alpha after this algorithm
-
-            return trajectory;
+            var simplified = [];
+            var d1 = trajectory[0].d.normalize();
+            var s1 = trajectory[0];
+            for(var i = 1; i < trajectory.length;i++)
+            {
+                var s2 = trajectory[i];
+                var d2 = trajectory[i].d.normalize();
+                var dot = Vector.prototype.dot(d1,d2);
+                var angle = Math.acos(dot);
+                if(angle>alpha)
+                {
+                    var nseg = new Segment(s1.p1,s2.p1);
+                    s1 = s2;
+                    d1 = d2;
+                    simplified.push(nseg);
+                }
+            }
+            var nseg = new Segment(s1.p1,trajectory[trajectory.length-1].p2);
+            simplified.push(nseg);
+            return simplified;
         }
     }
 }
