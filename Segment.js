ded158ec688bb1e708a4b88ecee4bb4f91de1968
Thom Hurks <thomhurks@gmail.com>
2015-04-17 20:09:12 +0200

    Code cleanup and optimizations in Segment.js

--- Segment.js
+++ Segment.js
@@ -1,16 +1,13 @@
 /**
  * Created by Helmond on 16-4-2015.
  */
-function Segment(p1,p2, weight)
-{
+function Segment(p1, p2, weight) {
+    "use strict";
     this.weight = 1;
 
-    if (weight != undefined) {
+    if (weight !== undefined) {
         this.weight = weight;
     }
-
-    this.p1;
-    this.p2;
 
     if (p1.x < p2.x) {
         this.p1 = p1;
@@ -20,75 +17,79 @@ function Segment(p1,p2, weight)
         this.p2 = p1;
     }
 
-    this.d;
     this.updateD();
     this.length = this.d.length();
 
     this.intervalTree = new IntervalTree();
     this.removed = false;
 
-    return(this);
+    return this;
 }
 
 
-Segment.prototype.updateD = function() {
+Segment.prototype.updateD = function () {
+    "use strict";
     this.d = this.p2.subtract(this.p1);
 };
 
-//Returns null if the lines are collinear, or partially overlap.
-//Otherwise, returns an array [t,u] such that the intersection point lies at this.getPointAt(t) or l.getPointAt(u)
+// Returns null if the lines are collinear, or partially overlap.
+// Otherwise, returns an array [t,u] such that the intersection point lies at this.getPointAt(t) or l.getPointAt(u)
 // http://stackoverflow.com/questions/563198/how-do-you-detect-where-two-line-segments-intersect
-Segment.prototype.getIntersection = function(l){//returns the intersection point between line l1 and l2
-    var A = this.p1;
-    var B = this.p2;
-    var C = l.p1;
-    var D = l.p2;
-    var p = A;
-    var r = B.subtract(A);
-    var q = C;
-    var s = D.subtract(C);
-    var qminp = q.subtract(p);
-    var rs = Vector.prototype.cross2D(r,s);
-    if(rs==0)//lines are colinear or paralel
-    {//We do not care if colinear lines overlap
-        return null ;
+// Returns the intersection point between line l1 and l2
+Segment.prototype.getIntersection = function (l) {
+    "use strict";
+    var A = this.p1,
+        B = this.p2,
+        C = l.p1,
+        D = l.p2,
+        p = A,
+        r = B.subtract(A),
+        q = C,
+        s = D.subtract(C),
+        qminp = q.subtract(p),
+        rs = Vector.prototype.cross2D(r, s),
+        t, u;
+    // Lines are colinear or parallel.
+    if (rs === 0) {
+        //We do not care if colinear lines overlap
+        return null;
     }
 
-    var t = Vector.prototype.cross2D(qminp,s)/rs;//if(t<0||t>1){return(null);}
-    var u = Vector.prototype.cross2D(qminp,r)/rs;//can be optimized by returning null if we already know t<0 || t>1
-    if(0 <= t && t<= 1 && 0<= u && u<= 1)
-    {
-        return([t,u]);
+    t = Vector.prototype.cross2D(qminp, s) / rs;
+    // Can be optimized by returning null if we already know t<0 || t>1
+    u = Vector.prototype.cross2D(qminp, r) / rs;
+    if (0 <= t && t <= 1 && 0 <= u && u <= 1) {
+        return ([t, u]);
     }
-    return null ;
+    return null;
 };
 
-Segment.prototype.clone = function() {
+Segment.prototype.clone = function () {
+    "use strict";
     return new Segment(this.p1.clone(), this.p2.clone());
 };
 
 //Returns the point on this line that lies at 100*t % from p1 to p2.
-Segment.prototype.getPointAt = function(t){
+Segment.prototype.getPointAt = function (t) {
+    "use strict";
     return this.p1.add(this.d.scale(t));
 };
 
 //Returns at what side of the line
 //http://stackoverflow.com/questions/2752725/finding-whether-a-point-lies-inside-a-rectangle-or-not
-Segment.prototype.sideOf = function(p){
-    var A = -(this.p2.y - this.p1.y);
-    var B = this.p2.x - this.p1.x;
-    var C = -(A * this.p1.x + B * this.p1.y)
-    return A * p.x + B * p.y + C
+Segment.prototype.sideOf = function (p) {
+    "use strict";
+    var A = -(this.p2.y - this.p1.y),
+        B = (this.p2.x - this.p1.x),
+        C = -(A * this.p1.x + B * this.p1.y);
+    return A * p.x + B * p.y + C;
 };
 
-Segment.prototype.projectOn = function(segment) {
-
-    var s1 = new Segment(this.p1, this.p2);
-    var s2 = new Segment(segment.p1, segment.p2);
-
-    //console.log(s1.toString());
-    //console.log(s2.toString());
-    //console.log('-------');
+Segment.prototype.projectOn = function (segment) {
+    "use strict";
+    var s1 = new Segment(this.p1, this.p2),
+        s2 = new Segment(segment.p1, segment.p2),
+        angle;
 
     // translate so s1.p1 is on origin
     s1.p2 = s1.p2.subtract(s1.p1);
@@ -97,11 +98,7 @@ Segment.prototype.projectOn = function(segment) {
     // must be done last
     s1.p1 = s1.p1.subtract(s1.p1);
 
-    //console.log(s1.toString());
-    //console.log(s2.toString());
-    //console.log('-------');
-
-    var angle = Math.atan(s1.p2.y/s1.p2.x);
+    angle = Math.atan(s1.p2.y / s1.p2.x);
 
     // s1 is now on the positive x-axis
     s1.p2.x = s1.p2.length();
@@ -111,11 +108,8 @@ Segment.prototype.projectOn = function(segment) {
     s2.p1 = s2.p1.rotate(-angle);
     s2.p2 = s2.p2.rotate(-angle);
 
-    //console.log(s1.toString());
-    //console.log(s2.toString());
-    //console.log('-------');
-
-    var epsBox = new Rectangle(new Vector(s1.p1.x, -EPS), new Vector(s1.p2.x, -EPS), new Vector(s1.p2.x, EPS), new Vector(s1.p1.x, EPS));
+    var epsBox = new Rectangle(new Vector(s1.p1.x, -EPS), new Vector(s1.p2.x, -EPS),
+                               new Vector(s1.p2.x, EPS),  new Vector(s1.p1.x, EPS));
     var interval = epsBox.getSubLineInside(s2);
 
     var result = {
@@ -125,51 +119,53 @@ Segment.prototype.projectOn = function(segment) {
 
     if (interval.length > 0) {
         s2.updateD();
-        var a = s2.getPointAt(interval[0]).x / s1.p2.x;
-        var b = s2.getPointAt(interval[1]).x / s1.p2.x;
+        var a = (s2.getPointAt(interval[0]).x / s1.p2.x),
+            b = (s2.getPointAt(interval[1]).x / s1.p2.x);
 
         result.segmentInterval = new Interval(interval[0], interval[1]);
-        result.baseInterval = new Interval(Math.min(a,b), Math.max(a,b));
+        result.baseInterval = new Interval(Math.min(a, b), Math.max(a, b));
     }
-
-    //console.log(result);
-
     return result;
 };
 
-Segment.prototype.toString = function() {
+Segment.prototype.toString = function () {
+    "use strict";
     return '<' + this.p1.toString() + '   ' + this.p2.toString() + '>';
 };
 
-Segment.prototype.toString = function() {
+Segment.prototype.toString = function () {
+    "use strict";
     return '<' + this.p1.toString() + '   ' + this.p2.toString() + '>';
-}
+};
 
-Segment.prototype.getSubSegments = function() {
+Segment.prototype.getSubSegments = function () {
+    "use strict";
+    //if (this.p1.x > -85.97 && this.p1.x < -85.96 && this.p1.y < -0.854 && this.p1.y > -0.855 ||
+    //    this.p2.x > -85.97 && this.p2.x < -85.96 && this.p2.y < -0.854 && this.p2.y > -0.855) {
+    //    console.log(this);
+    //}
 
-    if (this.p1.x > -85.97 && this.p1.x < -85.96 && this.p1.y < -0.854 && this.p1.y > -0.855 || this.p2.x > -85.97 && this.p2.x < -85.96 && this.p2.y < -0.854 && this.p2.y > -0.855) {
-        console.log(this);
-    }
+    var intervals = this.intervalTree.intervals,
+        segments = [],
+        points = [],
+        i;
 
-    var intervals = this.intervalTree.intervals;
-    var segments = [];
-
-    var points = [];
     points.push(this.getPointAt(intervals[0].start));
 
-    if (this.p1.x > -85.97 && this.p1.x < -85.96 && this.p1.y < -0.854 && this.p1.y > -0.855 || this.p2.x > -85.97 && this.p2.x < -85.96 && this.p2.y < -0.854 && this.p2.y > -0.855) {
-        console.log(points);
-    }
+    //if (this.p1.x > -85.97 && this.p1.x < -85.96 && this.p1.y < -0.854 && this.p1.y > -0.855 ||
+    //    this.p2.x > -85.97 && this.p2.x < -85.96 && this.p2.y < -0.854 && this.p2.y > -0.855) {
+    //    console.log(points);
+    //}
 
-    for (var i = 0; i < intervals.length; i++) {
+    for (i = 0; i < intervals.length; i++) {
         points.push(this.getPointAt(intervals[i].end));
 
-        if (this.p1.x > -85.97 && this.p1.x < -85.96 && this.p1.y < -0.854 && this.p1.y > -0.855 || this.p2.x > -85.97 && this.p2.x < -85.96 && this.p2.y < -0.854 && this.p2.y > -0.855) {
-            console.log(segments);
-        }
+        //if (this.p1.x > -85.97 && this.p1.x < -85.96 && this.p1.y < -0.854 && this.p1.y > -0.855 ||
+        //    this.p2.x > -85.97 && this.p2.x < -85.96 && this.p2.y < -0.854 && this.p2.y > -0.855) {
+        //    console.log(segments);
+        //}
 
-        segments.push(new Segment(points[i], points[i+1], intervals[i].weight));
+        segments.push(new Segment(points[i], points[i + 1], intervals[i].weight));
     }
-
     return segments;
-}
+};
