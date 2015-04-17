5ec3a1d1db2ea40eedbaf5784d9c6e45652f03df
Thom Hurks <thomhurks@gmail.com>
2015-04-17 23:05:37 +0200

    Handle debug menu input.

--- debugmenu.js
+++ debugmenu.js
@@ -4,30 +4,52 @@ var DebugMenu = L.Control.extend({
     },
     onAdd: function (map) {
         "use strict";
-        var container = L.DomUtil.create('div', 'dev-menu');
+        var container = L.DomUtil.create('div', 'dev-menu-container');
 
-        d3.select(container).append("div")
+        var devMenu = d3.select(container).append("div")
             .attr("id", "dev-menu")
             .text("test test test")
+            // Commence dirty inline HTML.
             .html('<div class="panel panel-default">\
                             <div class="panel-heading">\
                                 <h3 class="panel-title">Debug Menu</h3>\
                             </div>\
                             <div class="panel-body">\
                                 <div class="input-group">\
-                                    <span class="input-group-addon" id="epsilon">&epsilon;</span>\
-                                    <input type="text" class="form-control" placeholder="' + EPS + '" aria-describedby="epsilon">\
+                                    <span class="input-group-addon">&epsilon;</span>\
+                                    <input type="text" class="form-control" id="epsilon" placeholder="' + EPS + '" aria-describedby="epsilon">\
                                 </div>\
                                 <div class="input-group">\
-                                    <span class="input-group-addon" id="epsilon_sim">&epsilon;<sub>s</sub></span>\
-                                    <input type="text" class="form-control" placeholder="' + EPS_S + '" aria-describedby="epsilon_sim">\
+                                    <span class="input-group-addon">&epsilon;<sub>s</sub></span>\
+                                    <input type="text" class="form-control" id="epsilon_sim" placeholder="' + EPS_S + '" aria-describedby="epsilon_sim">\
                                 </div>\
                                 <div class="input-group">\
-                                    <span class="input-group-addon" id="alpha">&alpha;</span>\
-                                    <input type="text" class="form-control" placeholder="' + ALPHA * (180/Math.PI) + '" aria-describedby="alpha">\
+                                    <span class="input-group-addon">&alpha;</span>\
+                                    <input type="text" class="form-control" id="alpha" placeholder="' + ALPHA * (180/Math.PI) + '" aria-describedby="alpha">\
                                 </div>\
                             </div>\
                            </div>');
+
+        var epsilon = devMenu.select("#epsilon");
+        var epsilon_sim = devMenu.select("#epsilon_sim");
+        var alpha = devMenu.select("#alpha");
+
+        console.log(epsilon);
+
+        epsilon.on("change", function () {
+            EPS = this.value;
+            console.log("EPS set to " + EPS.toString());
+        });
+
+        epsilon_sim.on("change", function () {
+            EPS_S = this.value;
+            console.log("EPS_S set to " + EPS_S.toString());
+        });
+
+        alpha.on("change", function () {
+            ALPHA = this.value;
+            console.log("ALPHA set to " + ALPHA.toString());
+        });
 
         return container;
     }
